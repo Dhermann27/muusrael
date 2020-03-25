956f277baeb4a2c5ed49589c3482ef6d43620b06
Dan Hermann <dh78@me.com>
2020-03-25 15:33:56 -0500

    Timezone

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -41,4 +41,14 @@ class Kernel extends ConsoleKernel
 
         require base_path('routes/console.php');
     }
+
+    /**
+     * Get the timezone that should be used by default for scheduled events.
+     *
+     * @return \DateTimeZone|string|null
+     */
+    protected function scheduleTimezone()
+    {
+        return 'America/Chicago';
+    }
 }
