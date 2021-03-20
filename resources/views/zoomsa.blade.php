0710ebc02bb6f7425a90013f1e5e190048599110
Dan Hermann <Dh78@me.com>
2021-03-20 10:44:30 -0500

    Link color fix

--- resources/views/zoomsa.blade.php
+++ resources/views/zoomsa.blade.php
@@ -14,20 +14,20 @@
 
                     <p>Attend the Virtual Awesome Choir rehearsal with Pam Blevins Hinkle on <strong>April 10 @ 10am CDT</strong><br/>
 
-                        <a style="color: -webkit-link; text-decoration: underline;" href="https://us02web.zoom.us/j/81271766995?pwd=b0VLSWQxOElYU3dTY3ZPRHBURkRiQT09">https://us02web.zoom.us/j/81271766995?pwd=b0VLSWQxOElYU3dTY3ZPRHBURkRiQT09</a><br/>
+                        <a style="color: #007bff; text-decoration: underline;" href="https://us02web.zoom.us/j/81271766995?pwd=b0VLSWQxOElYU3dTY3ZPRHBURkRiQT09">https://us02web.zoom.us/j/81271766995?pwd=b0VLSWQxOElYU3dTY3ZPRHBURkRiQT09</a><br/>
 
                         Meeting ID: 812 7176 6995<br/>
 
                         Passcode: awesome<br/>
 
-                        Submit a Virtual Awesome Choir video by June 1: <a style="color: -webkit-link; text-decoration: underline;" href="https://forms.gle/YqupFGgdQpM3BEmN9">https://forms.gle/YqupFGgdQpM3BEmN9</a>
+                        Submit a Virtual Awesome Choir video by June 1: <a style="color: #007bff; text-decoration: underline;" href="https://forms.gle/YqupFGgdQpM3BEmN9">https://forms.gle/YqupFGgdQpM3BEmN9</a>
                     </p>
 
                     <p>Submit a video greeting or farewell which will be used in our Opening/Closing Celebrations by
-                        June 1: <a style="color: -webkit-link; text-decoration: underline;" href="https://forms.gle/wWrgiBL4Xr6ZL7hi6">https://forms.gle/wWrgiBL4Xr6ZL7hi6</a>
+                        June 1: <a style="color: #007bff; text-decoration: underline;" href="https://forms.gle/wWrgiBL4Xr6ZL7hi6">https://forms.gle/wWrgiBL4Xr6ZL7hi6</a>
                     </p>
 
-                    <p>Sign up for Coffee House by June 30: <a style="color: -webkit-link; text-decoration: underline;" href="https://forms.gle/XBxde4FEKpq9HnBe8">https://forms.gle/XBxde4FEKpq9HnBe8</a>
+                    <p>Sign up for Coffee House by June 30: <a style="color: #007bff; text-decoration: underline;" href="https://forms.gle/XBxde4FEKpq9HnBe8">https://forms.gle/XBxde4FEKpq9HnBe8</a>
                     </p>
                 </div>
             </div>
