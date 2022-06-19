34312d910b13d2dce431a5f6dd9640a5ccdfd6df
Dan Hermann <Dh78@me.com>
2022-06-19 17:42:37 -0500

    Nosteps

--- app/Http/Controllers/ConfirmController.php
+++ app/Http/Controllers/ConfirmController.php
@@ -75,8 +75,7 @@ class ConfirmController extends Controller
 //            $request->session()->flash('warning', 'You have no campers registered for this year.');
 //            return redirect()->action('CamperController@index');
 //        }
-        return view('confirm', ['families' => ThisyearFamily::where('id', Auth::user()->camper->family_id)->get(),
-            'steps' => $this->getSteps()]);
+        return view('confirm', ['families' => ThisyearFamily::where('id', Auth::user()->camper->family_id)->get()]);
 
     }
 
