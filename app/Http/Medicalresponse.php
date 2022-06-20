cb603bd04e577523162c105d28f30084e2cd0e87
Dan Hermann <Dh78@me.com>
2022-06-19 19:00:02 -0500

    Very close?

--- app/Http/Medicalresponse.php
+++ app/Http/Medicalresponse.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Medicalresponse extends Model
 {
+    protected $fillable = ['yearattending_id'];
     public function yearattending()
     {
         return $this->hasOne('App\Http\Yearattending');
