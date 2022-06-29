ba89bf3215a2cec9ce5664f68277c0f6f28868af
Dan Hermann <Dh78@me.com>
2022-06-29 16:56:51 -0500

    Timeslot page

--- resources/views/reports/workshops.blade.php
+++ resources/views/reports/workshops.blade.php
@@ -16,54 +16,59 @@
         @foreach($timeslots as $timeslot)
             <div class="tab-pane fade{!! $loop->first ? ' active show' : '' !!}" id="tab-{{ $timeslot->id }}"
                  role="tabpanel">
-                <div style="page-break-inside: avoid">
                 @if($timeslot->id != \App\Enums\Timeslotname::Excursions)
-                    <h5>{{ $timeslot->start_time->format('g:i A') }} - {{ $timeslot->end_time->format('g:i A') }}</h5>
+                    <div class="d-none d-print-block display-1">{{ $timeslot->name }}</div>
+                    <div class="d-none d-print-block display-2">{{ $timeslot->start_time->format('g:i A') }}
+                        - {{ $timeslot->end_time->format('g:i A') }}</div>
+                    <h5 class="d-print-none">{{ $timeslot->start_time->format('g:i A') }}
+                        - {{ $timeslot->end_time->format('g:i A') }}</h5>
+                    <div class="page-break"></div>
                 @endif
                 @foreach($timeslot->workshops->where('year_id', $year->id) as $workshop)
-                    <h4>{!! $workshop->name !!} ({{ count($workshop->choices) }} / {{ $workshop->capacity }})</h4>
-                    <table class="table">
-                        <thead>
-                        <tr>
-                            <th width="50%">Name</th>
-                            <th width="25%">Sign Up Date</th>
-                            <th width="25%">Controls</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($workshop->choices()->orderBy('is_leader', 'desc')->orderBy('created_at')->get() as $choice)
-                            <tr @if($choice->is_enrolled == '0') class="table-danger"@endif>
-                                <td>{{ $choice->yearattending->camper->lastname }},
-                                    {{ $choice->yearattending->camper->firstname }}</td>
-                                <td>
-                                    @if($choice->is_leader == '1')
-                                        <strong>Leader</strong>
-                                    @else
-                                        {{ $choice->created_at }}
-                                    @endif
-                                </td>
-                                <td>
-                                    @include('includes.admin.controls', ['id' => $choice->yearattending->camper->id])
+                    <div style="page-break-inside: avoid">
+                        <h4>{!! $workshop->name !!} ({{ count($workshop->choices) }} / {{ $workshop->capacity }})</h4>
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th width="50%">Name</th>
+                                <th width="25%">Sign Up Date</th>
+                                <th width="25%">Controls</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($workshop->choices()->orderBy('is_leader', 'desc')->orderBy('created_at')->get() as $choice)
+                                <tr @if($choice->is_enrolled == '0') class="table-danger"@endif>
+                                    <td>{{ $choice->yearattending->camper->lastname }},
+                                        {{ $choice->yearattending->camper->firstname }}</td>
+                                    <td>
+                                        @if($choice->is_leader == '1')
+                                            <strong>Leader</strong>
+                                        @else
+                                            {{ $choice->created_at }}
+                                        @endif
+                                    </td>
+                                    <td>
+                                        @include('includes.admin.controls', ['id' => $choice->yearattending->camper->id])
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                            <tfoot>
+                            <tr class="d-print-none">
+                                <td colspan="3">Distribution list: {{ $workshop->emails }}
                                 </td>
                             </tr>
-                        @endforeach
-                        </tbody>
-                        <tfoot>
-                        <tr class="d-print-none">
-                            <td colspan="3">Distribution list: {{ $workshop->emails }}
-                            </td>
-                        </tr>
-                        <tr class="d-none d-print-block">
-                            <td colspan="3"><h4>Walk Ins:</h4></td>
-                        </tr>
-                        @for($i=count($workshop->choices); $i<min($workshop->capacity, count($workshop->choices)+5); $i++)
                             <tr class="d-none d-print-block">
-                                <td colspan="2" style="border-bottom: 1px solid black;">&nbsp;</td>
-                                <td>&nbsp;</td>
+                                <td colspan="3"><h4>Walk Ins:</h4></td>
                             </tr>
-                        @endfor
-                        </tfoot>
-                    </table>
+                            @for($i=count($workshop->choices); $i<min($workshop->capacity, count($workshop->choices)+5); $i++)
+                                <tr class="d-none d-print-block">
+                                    <td colspan="2" style="border-bottom: 1px solid black;">&nbsp;</td>
+                                    <td>&nbsp;</td>
+                                </tr>
+                            @endfor
+                            </tfoot>
+                        </table>
                     </div>
                     <div class="page-break"></div>
                 @endforeach
