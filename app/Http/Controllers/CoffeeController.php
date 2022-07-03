8c9964c4eab705f88aec9bc44fd037bb685c2555
Dan Hermann <Dh78@me.com>
2022-07-03 16:44:01 -0500

    Once again packages

--- app/Http/Controllers/CoffeeController.php
+++ app/Http/Controllers/CoffeeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Coffeehouseact;
 use App\Http\Year;
-use App\Models\ThisyearCamper;
+use App\Http\ThisyearCamper;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
